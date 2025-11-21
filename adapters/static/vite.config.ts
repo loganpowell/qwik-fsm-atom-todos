b1a2b7825b16ab8ef60dfb92001f1775a4fb5f81
loganpowell <loganp@tepper.cmu.edu>
2025-11-21 12:42:48 -0500

    fix: build gh pages

--- adapters/static/vite.config.ts
+++ adapters/static/vite.config.ts
@@ -4,7 +4,6 @@ import { staticAdapter } from "@builder.io/qwik-city/adapters/static/vite";
 
 export default extendConfig(baseConfig, () => {
   return {
-    base: "/qwik-fsm-atom-todos/",
     build: {
       ssr: true,
       rollupOptions: {
